41250275024827b5aee38a5c962a83732388980e
Devineni,Sai Sirisha <S531367@nwmissouri.edu>
2019-02-26 13:37:59 -0600

    Added Models/Degree.cs Models/Requirement.cs and made corrections to Models/DegreePlan.cs

--- Team01Section01Project/Team01Section01Project/Models/DegreePlan.cs
+++ Team01Section01Project/Team01Section01Project/Models/DegreePlan.cs
@@ -1,14 +1,17 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Team01Section01Project.Models
 {
     public class DegreePlan
     {
         public int DegreePlanID { get; set; }
-        public int Degreeid { get; set; }
-        public int studentid { get; set; }
-        public String DegreePlanAbrevation { get; set; }
+        public int DegreeID { get; set; }
+        public int StudentID { get; set; }
+        [StringLength(8, MinimumLength = 3)]
+        public String DegreePlanAbrev { get; set; }
+        [StringLength(20, MinimumLength = 3)]
         public String DegreePlanName { get; set; }
     }
 }
